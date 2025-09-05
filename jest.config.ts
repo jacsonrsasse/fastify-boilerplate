ca1e32ba856c234050817c54ad51cef4ac7595f3
Jacson R. Sasse <jacson.sasse@hotmail.com>
2025-09-05 01:54:31 -0300

    feat: added jest for tests

---
+++ jest.config.ts
@@ -0,0 +1,16 @@
+import type { Config } from 'jest';
+
+const config: Config = {
+  verbose: true,
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.(t|j)s$': 'ts-jest',
+  },
+  moduleFileExtensions: ['js', 'json', 'ts'],
+  rootDir: '.',
+  testRegex: '.*\\.spec\\.ts$',
+  coverageDirectory: '../coverage',
+};
+
+export default config;
