428aca262547fa6924ef1cbdfe73061d9f71fa30
Jacson R. Sasse <jacson.sasse@hotmail.com>
2025-09-06 01:27:06 -0300

    feat: added swagger schema description

--- src/routes/any.routes.ts
+++ src/routes/any.routes.ts
@@ -1,6 +1,21 @@
+import z from 'zod';
 import * as anyController from '../controllers/any.controller';
 import { FastifyTypedInstance } from '../types/fastify';
 
 export async function anyRoutes(server: FastifyTypedInstance) {
-  server.get('/', anyController.anyGetRouter);
+  server.get(
+    '/',
+    {
+      schema: {
+        tags: ['Any'],
+        description: 'Any description here',
+        response: {
+          200: z.object({
+            hello: z.string(),
+          }),
+        },
+      },
+    },
+    anyController.anyGetRouter,
+  );
 }
