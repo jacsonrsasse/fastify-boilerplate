60700c06fc1a07445804dd4728f6a61cbc9b1d52
Jacson R. Sasse <jacson.sasse@hotmail.com>
2025-09-06 01:12:20 -0300

    test: fixed test, now it works

--- src/controllers/any.controller.spec.ts
+++ src/controllers/any.controller.spec.ts
@@ -1,12 +1,18 @@
 import supertest from 'supertest';
 import app from '../app';
 
-const request = supertest(app.server);
-
 describe('anyController', () => {
+  afterEach(() => {
+    app.close();
+  });
+
   it('should return hello world', async () => {
-    const response = await request.get('/any');
-    expect(response.status).toBe(200);
-    expect(response.body).toEqual({ hello: 'world' });
+    const res = await app.inject({
+      method: 'GET',
+      url: '/any',
+    });
+
+    expect(res.statusCode).toBe(200);
+    expect(JSON.parse(res.payload)).toEqual({ hello: 'world' });
   });
 });
