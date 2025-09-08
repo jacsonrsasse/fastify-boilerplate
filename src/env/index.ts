2c7136cfb8fc65af149dce9e710826a4ad877544
Jacson R. Sasse <jacson.sasse@hotmail.com>
2025-09-08 00:34:07 -0300

    feat: DATABASE_URL variable and handle gracefully shut down for docker containers

--- src/env/index.ts
+++ src/env/index.ts
@@ -3,6 +3,7 @@ import z from 'zod';
 
 const envSchema = z.object({
   PORT: z.coerce.number(),
+  DATABASE_URL: z.string(),
 });
 
 type Env = z.infer<typeof envSchema>;
--- src/server.ts
+++ src/server.ts
@@ -3,7 +3,7 @@ import { env } from './env';
 
 const run = async () => {
   try {
-    await app.listen({ port: env.PORT });
+    await app.listen({ port: env.PORT, host: '0.0.0.0' });
   } catch (err) {
     app.log.error(err);
     process.exit(1);
@@ -11,3 +11,12 @@ const run = async () => {
 };
 
 run();
+
+const closeGracefully = async (signal: string) => {
+  console.log(`\nReceived signal ${signal}, closing...`);
+  await app.close();
+  process.exit(0);
+};
+
+process.on('SIGTERM', closeGracefully);
+process.on('SIGINT', closeGracefully);
